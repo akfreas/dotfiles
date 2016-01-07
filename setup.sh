de4a10302cc75bb59a34dcc77e4127cb96eebfca
Alexander Freas <alex@komoot.de>
2016-01-07 15:20:27 +0100

    use script path to setup links.

--- setup.sh
+++ setup.sh
@@ -1,21 +1,24 @@
+SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
+echo $SCRIPTPATH
+exit
 mv ~/.bash_profile ~/.bash_profile.bak
-ln -s ./bash/.bash_profile ~/.bash_profile
+ln -s $SCRIPTPATH/bash/.bash_profile ~/.bash_profile
 
 if [ -d "~/Library/Preferences/com.googlecode.iterm2.plist" ];
 then
     rm ~/Library/Preferences/com.googlecode.iterm2.plist
-    ln -s ./iTerm/com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist
+    ln -s $SCRIPTPATH/iTerm/com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist
 fi
 
 if [ -d "~/Library/Developer/Xcode/UserData" ];
 then
     rm -rf ~/Library/Developer/Xcode/UserData
-    ln -s ./Xcode/UserData  ~/Library/Developer/Xcode/UserData
+    ln -s $SCRIPTPATH/Xcode/UserData  ~/Library/Developer/Xcode/UserData
 fi
 
 mv ~/.vimrc ~/.vimrc-bak
-ln -s ./vim ~/.vim
-ln -s ./vim/.vimrc ~/.vimrc
+ln -s $SCRIPTPATH/vim ~/.vim
+ln -s $SCRIPTPATH/vim/.vimrc ~/.vimrc
 
 mv ~/.gitconfig ~/.gitconfig.bak
-ln -s ./git/.gitconfig ~/.gitconfig
+ln -s $SCRIPTPATH/git/.gitconfig ~/.gitconfig
