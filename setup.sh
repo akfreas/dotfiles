a14ee6b6b91b66fae15a78b048ba5d446c5a384d
Alexander Freas <akfreas@gmail.com>
2016-07-22 10:41:31 +0000

    improved setup script and split apt-packages.

--- setup.sh
+++ setup.sh
@@ -1,8 +1,7 @@
 SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
 echo $SCRIPTPATH
 
-mv ~/.bash_profile ~/.bash_profile.bak
-ln -s $SCRIPTPATH/.zshrc ~/.bash_profile
+sudo su 
 
 if [ -d "~/Library/Preferences/com.googlecode.iterm2.plist" ];
 then
@@ -36,36 +35,38 @@ wget -O - https://raw.githubusercontent.com/nvbn/thefuck/master/install.sh | sh 
 unamestr=`uname`
 if [[ "$unamestr" == 'Linux' ]]; then
 
-    sudo apt-get remove --yes vim vim-runtime gvim vim-tiny vim-common vim-gui-common
+    sudo apt-get remove --yes vim vim-runtime gvim vim-tiny vim-common vim-gui-common vim-nox
 
-    sudo apt-get install --yes rbenv
-    sudo apt-get install --yes checkinstall 
-    sudo apt-get install --yes cmake
-    sudo apt-get install --yes ruby ruby-dev libncurses5-dev mercurial build-essential rake
-    sudo apt-get install --yes docker.io
 
-    cd `mktemp`
+    cat ./apt-packages.txt | xargs sudo apt-get --yes --force-yes install
+
+    cd ~
     git clone https://github.com/vim/vim.git
     cd vim
     ./configure --with-features=huge \
-            --enable-multibyte \
-            --enable-rubyinterp \
-            --enable-pythoninterp \
-            --with-python-config-dir=/usr/lib/python2.7/config \
-            --enable-perlinterp \
-            --enable-luainterp \
-            --enable-gui=gtk2 --enable-cscope --prefix=/usr
+                --enable-multibyte \
+                --enable-rubyinterp \
+                --enable-pythoninterp \
+                --with-python-config-dir=/usr/lib/python2.7/config \
+                --enable-python3interp \
+                --with-python3-config-dir=/usr/lib/python3.5/config \
+                --enable-perlinterp \
+                --enable-luainterp \
+                --enable-gui=gtk2 --enable-cscope --prefix=/usr
     make VIMRUNTIMEDIR=/usr/share/vim/vim74
+    sudo make install
+    cd ~
     sudo checkinstall
     sudo update-alternatives --install /usr/bin/editor editor /usr/bin/vim 1
     sudo update-alternatives --set editor /usr/bin/vim
     sudo update-alternatives --install /usr/bin/vi vi /usr/bin/vim 1
     sudo update-alternatives --set vi /usr/bin/vim
 
-    apt-get install zsh
-    apt-get install git-core
-    wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+    TEMP_PAM=`mktemp`; echo "auth       sufficient   pam_wheel.so trust group=chsh" | cat - /etc/pam.d/chsh >  $TEMP_PAM && sudo mv $TEMP_PAM /etc/pam.d/chsh
+    sudo groupadd chsh
+    usermod -a -G chsh `whoami`
     chsh -s `which zsh`
+#    wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
 
 
     # Docker setup
@@ -82,10 +83,11 @@ if [[ "$unamestr" == 'Linux' ]]; then
 
 elif [[ "$unamestr" == 'Darwin' ]]; then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
+    wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+    sudo chsh -s `which zsh` `whoami`
 
-wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
-sudo chsh -s `which zsh` `whoami`
+
+fi
 
 # Install Vim Plugins
 
@@ -94,11 +96,20 @@ cd ~/.vim/bundle/YouCompleteMe
 ./install.py --clang-completer
 
 # Pip Install
-
+curl https://bootstrap.pypa.io/get-pip.py | sudo python
 sudo pip install virtualenvwrapper
 
 # Docker Compose
 
-curl -L https://github.com/docker/compose/releases/download/1.6.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+cd `mktemp`
+curl -L https://github.com/docker/compose/releases/download/1.7.1/docker-compose-`uname -s`-`uname -m` > ./docker-compose
+sudo mv docker-compose /usr/local/bin/docker-compose
+cd ~
+sudo chmod +x /usr/local/bin/docker-compose
 
+mv ~/.bash_profile ~/.bash_profile.bak
+ln -s $SCRIPTPATH/.zshrc ~/.bash_profile
+
+
+mv ~/.zshrc ~/.zshrc.bak
+ln -s $SCRIPTPATH/.zshrc ~/.zshrc
