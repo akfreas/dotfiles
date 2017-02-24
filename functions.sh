2c77421ecf17fc5ae30654a7ca48e97c73ec0de2
Alexander Freas <akfreas@gmail.com>
2017-02-24 11:27:13 +0100

    Updated find_replace function to use updated script, added target dir parameter in rename

--- functions.sh
+++ functions.sh
@@ -1,21 +1,5 @@
 function find_replace() {
-    FILES_MOVED=[0]=""
-    for f in `find . -type f  -exec grep -l $1 {} ";"`; do
-        sed -i "" "s/$1/$2/g" $f;
-        echo "Replaced $1 with $2 in $f."
-    done;
-
-
-    let x=0;
-    for f in `find . -name "*$1*"`; do
-       #echo " $f ${f/$1*./$2.}"
-       NEW=`dirname $f`"/"${f/$1*./$2.}
-       let "x+=1";
-       mv $f $NEW
-       echo "Moved $f to $NEW."
-   done; 
-   echo $x
-   echo $FILES_MOVED
+    sh $BASH_PROFILE_HOME/rename.sh $1 $2 $3
 }
 
 function playsound() {
--- rename.sh
+++ rename.sh
@@ -1,14 +1,18 @@
 BACKUP_DIR='_backup';
-FILE_LIST=`find . -type f -not -path "*$BACKUP_DIR*"`
+TARGET_DIR=$1;
+SEARCH_STRING=$2;
+REPLACE_STRING=$3;
+
+FILE_LIST=`find $TARGET_DIR -type f -not -path "*$BACKUP_DIR*"`
 mkdir -pv $BACKUP_DIR;
 while IFS= read -r line
 do
-    echo $line;
+  echo $line;
   if ! [[ " $list " =~ " $line " ]] ; then
-      LC_ALL=C sed -i '' "s%$1%$2%g" "$line";
+      LC_ALL=C sed -i '' "s%$SEARCH_STRING%$REPLACE_STRING%g" "$line";
   fi
-  if [[ $line == *"$1"* ]]; then
-      newfile=`echo $line | sed "s%$1%$2%g"`; 
+  if [[ $line == *"$SEARCH_STRING"* ]]; then
+      newfile=`echo $line | sed "s%$SEARCH_STRING%$REPLACE_STRING%g"`; 
       mkdir -pv "`dirname "$newfile"`"; 
       echo "Copying $line to $newfile";
       cp "$line" "$newfile" 2> /dev/null;
